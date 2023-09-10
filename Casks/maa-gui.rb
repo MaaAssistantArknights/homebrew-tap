3e29750da889d7d4dc5ee40e3c0ba806919b3671
Loong <wangl.cc@outlook.com>
2023-09-11 00:39:02 +0800

    fix lint

--- Casks/maa-gui.rb
+++ Casks/maa-gui.rb
@@ -1,11 +1,12 @@
 cask "maa-gui" do
+  version "4.23.3"
+  sha256 "214d4cab70f6a9eb10543fb0b59169bb3f8f7c2f6b9efd27154f3de2bbc834d0"
+
+  url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg"
   name "MAA.app"
   desc "Mac GUI for MAA (MaaAssistantArknights)"
-  homepage "https://maa.plus"
+  homepage "https://maa.plus/"
 
-  version "4.23.3"
-  url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg"
-  sha256 "214d4cab70f6a9eb10543fb0b59169bb3f8f7c2f6b9efd27154f3de2bbc834d0"
   app "MAA.app"
 
   conflicts_with cask: "MaaAssistantArknights/tap/maa-gui-beta"

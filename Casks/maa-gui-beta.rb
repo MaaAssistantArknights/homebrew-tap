777c2064c84142c6b9edab6910d73e79befb4eab
Loong <wangl.cc@outlook.com>
2023-09-11 01:11:04 +0800

    fix and depends_on macos

--- Casks/maa-gui-beta.rb
+++ Casks/maa-gui-beta.rb
@@ -3,12 +3,13 @@ cask "maa-gui-beta" do
   sha256 "d88d1cecd2207725b7fbb2fa1053902e90a6da1ba2bf46eabd747e10aadd69f4"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
-    verified "github.com/MaaAssistantArknights/MaaAssistantArknights/"
+    verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"
   name "MAA.app"
   desc "GUI for MAA (MaaAssistantArknights)"
   homepage "https://maa.plus/"
 
   conflicts_with cask: "MaaAssistantArknights/tap/maa-gui"
+  depends_on macos: ">= :big_sur"
 
   app "MAA.app"
 end

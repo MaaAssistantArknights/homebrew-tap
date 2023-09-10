cb28268f6812e61f11aa2d2adf58e0592df2b8b1
Loong <wangl.cc@outlook.com>
2023-09-11 01:18:04 +0800

    feat: add cask `maa-gui` and `maa-gui-beta` (#2)

---
+++ Casks/maa-gui.rb
@@ -0,0 +1,15 @@
+cask "maa-gui" do
+  version "4.23.3"
+  sha256 "214d4cab70f6a9eb10543fb0b59169bb3f8f7c2f6b9efd27154f3de2bbc834d0"
+
+  url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
+      verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"
+  name "MAA.app"
+  desc "GUI for MAA (MaaAssistantArknights)"
+  homepage "https://maa.plus/"
+
+  conflicts_with cask: "MaaAssistantArknights/tap/maa-gui-beta"
+  depends_on macos: ">= :big_sur"
+
+  app "MAA.app"
+end

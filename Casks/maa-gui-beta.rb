77525b30061667dfd49d036e11c444655e838b63
Loong <wangl.cc@outlook.com>
2023-09-18 14:45:38 +0800

    maa-gui-beta 4.24.0-beta.4 (#9)

--- Casks/maa-gui-beta.rb
+++ Casks/maa-gui-beta.rb
@@ -1,6 +1,6 @@
 cask "maa-gui-beta" do
-  version "4.24.0-beta.3"
-  sha256 "c4d612bcada1e47b616f32d92ca11d2d43ea49881da0eebd13b267d070b543b4"
+  version "4.24.0-beta.4"
+  sha256 "6b4e9a7a2f277941dd76a9ea9a5b1abf77f6c9136ddebac7e2380319f4bae59a"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
       verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"

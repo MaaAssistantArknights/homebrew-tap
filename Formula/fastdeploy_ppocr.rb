c26a296e6be8e116421533482af0849955971907
Loong <wangl.cc@outlook.com>
2023-10-29 16:37:41 +0800

    update to latest version and build without PRINT_LOG

--- Formula/fastdeploy_ppocr.rb
+++ Formula/fastdeploy_ppocr.rb
@@ -1,11 +1,14 @@
 class FastdeployPpocr < Formula
   desc "Stripped-down version of PaddlePaddle/FastDeploy"
   homepage "https://github.com/MaaAssistantArknights/FastDeploy"
-  # bug: terminate by SIGTRAP for this version if PRINT_LOG is OFF
-  url "https://github.com/MaaAssistantArknights/FastDeploy/archive/2716bc9a3e57df87dd492be3b82a352ef0f12684.tar.gz"
-  version "2023.10.11"
-  sha256 "83a3748a4612931f39a0e7464760f27f65e01ca5b65467f045d8379b7069c6f5"
+  url "https://github.com/MaaAssistantArknights/FastDeploy/archive/d0b018ac6c3daa22c7b55b555dc927a5c734d430.tar.gz"
+  version "2023.10.29"
+  sha256 "4a74b0f90178384124a97324e86edd4aa0fed44ac280e23cf3454513b14e0a6a"
   license "Apache-2.0"
+
+  livecheck do
+    skip "This formula is not tagged, so there is no version to check"
+  end
 
   depends_on "cmake" => :build
   depends_on "eigen" => :build
@@ -15,8 +18,6 @@ class FastdeployPpocr < Formula
 
   def install
     cmake_args = %w[
-      -DPRINT_LOG=ON
-      -DCMAKE_BUILD_TYPE=None
       -DBUILD_SHARED_LIBS=ON
     ]
     system "cmake", "-S", ".", "-B", "build", *cmake_args, *std_cmake_args

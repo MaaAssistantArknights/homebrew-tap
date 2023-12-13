3596d3cae049a513be2338c710c70600126d62d9
Loong <wangl.cc@outlook.com>
2023-12-13 06:41:13 +0000

    maa-cli-beta: add 0.4.0-beta.2 bottle.

--- Formula/maa-cli-beta.rb
+++ Formula/maa-cli-beta.rb
@@ -10,6 +10,13 @@ class MaaCliBeta < Formula
     regex(/^v?(\d+\.\d+\.\d+(?:-(?:beta|rc)\.\d+)?)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/MaaAssistantArknights/homebrew-tap/releases/download/maa-cli-beta-0.4.0-beta.2"
+    sha256 cellar: :any,                 ventura:      "7184b2dbc26df91031f4c23a0e7c49a21e8f8e31c895e23cdfe85b39223abc7a"
+    sha256 cellar: :any,                 monterey:     "e27dc26406961c2270f738e6a5fcb1e312a6a1de2e126fc2e49e0b3652994a6b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2bde7b72ade58485e63c3505d978417ac8b7173b9994f8ea5d31b9dfc197f3e7"
+  end
+
   option "without-git2", "Don't build with libgit2 resource updater backend"
   option "without-core-installer", "Don't build with core installer"
 

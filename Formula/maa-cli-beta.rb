5668c6e92569f6ff1d29432512a5a3b91e9d00ca
Loong <wangl.cc@outlook.com>
2023-12-13 13:54:19 +0800

    maa-cli-beta: new formula

---
+++ Formula/maa-cli-beta.rb
@@ -0,0 +1,51 @@
+class MaaCliBeta < Formula
+  desc "Command-line tool for MAA (MaaAssistantArknights)"
+  homepage "https://github.com/MaaAssistantArknights/maa-cli/"
+  url "https://github.com/MaaAssistantArknights/maa-cli/archive/refs/tags/v0.4.0-beta.2.tar.gz"
+  sha256 "2744d0a5a0d337e3490f0fe63126c3ddf51abc11c4ffeba92b7dae9e56449fe0"
+  license "AGPL-3.0-or-later"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+\.\d+\.\d+(?:-(?:beta|rc)\.\d+)?)$/i)
+  end
+
+  option "without-git2", "Don't build with libgit2 resource updater backend"
+  option "without-core-installer", "Don't build with core installer"
+
+  depends_on "rust" => :build
+
+  # openssl is always required on Linux
+  # while it's only required on macOS when building with git2
+  depends_on "openssl" if OS.linux? || build.with?("git2")
+  uses_from_macos "zlib"
+
+  conflicts_with "maa-cli", { because: "both provide maa" }
+
+  def install
+    ENV["CARGO_PROFILE_RELEASE_CODEGEN_UNITS"] = "1"
+    ENV["CARGO_PROFILE_RELEASE_LTO"] = "true"
+    ENV["CARGO_PROFILE_RELEASE_STRIP"] = "true"
+
+    # patch version
+    inreplace "maa-cli/Cargo.toml" do |s|
+      s.sub!(/(version\s*=\s*)"[^"]+"/, "version = \"#{version}\"")
+    end
+    inreplace "Cargo.lock" do |s|
+      s.sub!(/name\s*=\s*"maa-cli"\s*\n\s*version\s*=\s*"[^"]+"/,
+        "name = \"maa-cli\"\nversion = \"#{version}\"")
+    end
+
+    features = []
+    features += ["git2"] if build.with? "git2"
+    features += ["core_installer"] if build.with? "core-installer"
+
+    system "cargo", "install", "--no-default-features",
+      "--features", features.join(","), *std_cargo_args(path: "maa-cli")
+    fish_completion.install "maa-cli/share/fish/vendor_completions.d/maa.fish"
+  end
+
+  test do
+    assert_match "maa #{version}", shell_output("#{bin}/maa --version")
+  end
+end

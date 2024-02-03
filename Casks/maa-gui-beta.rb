1a4377df16c85986886367645aa992bc728dced9
runner <runner@Mac-1706920226604.local>
2024-02-03 00:39:09 +0000

    maa-gui-beta 5.0.0
    Closes #107.
    
    Signed-off-by: Loong <wangl.cc@outlook.com>

--- Casks/maa-gui-beta.rb
+++ Casks/maa-gui-beta.rb
@@ -1,6 +1,6 @@
 cask "maa-gui-beta" do
-  version "5.0.0-beta.2"
-  sha256 "f5cd3cda108bf1f7ce0bcf4438d779993d11164b6a4f594a98c324fe2c9665da"
+  version "5.0.0"
+  sha256 "1437709a808a90dc7928985e73d432b208e049ed97cdb7f92746ed91a8ea1ef6"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
       verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"

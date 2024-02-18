f3775465f51e2f76175e23b523458740b05d9d9e
runner <runner@Mac-1708240473049.local>
2024-02-18 08:08:42 +0000

    maa-cli-bin 0.4.3
    Closes #118.
    
    Signed-off-by: Loong <40141251+wangl-cc@users.noreply.github.com>

--- Casks/maa-cli-bin.rb
+++ Casks/maa-cli-bin.rb
@@ -1,6 +1,6 @@
 cask "maa-cli-bin" do
-  version "0.4.2"
-  sha256 "30a2b49d3dee54f367489945e3851e8c972f11a9d76a32e34fcb32eb342cda75"
+  version "0.4.3"
+  sha256 "f7aaca01b10fc7618c7ad08e7c44292807394e07e7ae2192c3752ff0768361d1"
 
   url "https://github.com/MaaAssistantArknights/maa-cli/releases/download/v#{version}/maa_cli-v#{version}-universal-apple-darwin.zip"
   name "maa-cli"

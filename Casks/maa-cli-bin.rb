1059ca6679b3bd05147c6875fbbce865c06686c1
runner <runner@Mac-1713863568714.local>
2024-04-23 09:15:23 +0000

    maa-cli-bin 0.4.7

--- Casks/maa-cli-bin.rb
+++ Casks/maa-cli-bin.rb
@@ -1,6 +1,6 @@
 cask "maa-cli-bin" do
-  version "0.4.6"
-  sha256 "c07b8195044ec913c33526df3b493b72b1c6075cefa0d05789f1ad27c569ad00"
+  version "0.4.7"
+  sha256 "d7157b95878bd74aabe3a7701ff9bafd22c2c5ec7579cad7eca61b9b2dcb9ea0"
 
   url "https://github.com/MaaAssistantArknights/maa-cli/releases/download/v#{version}/maa_cli-v#{version}-universal-apple-darwin.zip"
   name "maa-cli"

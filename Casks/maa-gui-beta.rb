d3ad3c796875eddb903bf337ac11249a7699add4
Loong <40141251+wangl-cc@users.noreply.github.com>
2024-10-01 02:48:41 -0700

    maa-gui-beta 5.7.0-beta.2 (#266)
    Bump `maa-gui-beta` to `5.7.0-beta.2`
    
    ---
    
    Created with `brew bump-cask-pr`.
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/maa-gui-beta.rb
+++ Casks/maa-gui-beta.rb
@@ -1,6 +1,6 @@
 cask "maa-gui-beta" do
-  version "5.7.0-beta.1"
-  sha256 "c192cf8edfb7e83316da14b5693e01fe68cd336b7f018fdf209224c440c2a921"
+  version "5.7.0-beta.2"
+  sha256 "f5e4572999ea7ba4a518233ac99e78faa147fa0bfba03166535e0e28b76f6a7b"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
       verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"

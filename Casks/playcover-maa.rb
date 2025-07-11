023df6650de1d26394353a48ea596ef7853d662c
Loong <git@loongw.cc>
2025-07-11 11:21:13 +0800

    playcover: add zap and change required arch and macos version

--- Casks/playcover-maa.rb
+++ Casks/playcover-maa.rb
@@ -13,7 +13,17 @@ cask "playcover-maa" do
 
   auto_updates true
   conflicts_with cask: "playcover-community"
-  depends_on macos: ">= :big_sur"
+  depends_on arch: :arm64
+  depends_on macos: ">= :monterey"
 
   app "PlayCover.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.playcover.PlayCover",
+    "~/Library/Caches/io.playcover.PlayCover",
+    "~/Library/Containers/io.playcover.PlayCover",
+    "~/Library/Frameworks/PlayTools.framework",
+    "~/Library/Preferences/io.playcover.PlayCover.plist",
+    "~/Library/Saved Application State/io.playcover.PlayCover.savedState",
+  ]
 end

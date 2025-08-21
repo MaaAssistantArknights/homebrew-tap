b2c685e0fcd32870cd1febe4a5655c1eaa400947
Loong <40141251+wangl-cc@users.noreply.github.com>
2025-08-21 17:24:43 +0800

    maa@beta: add cask maa@beta and deprecate gui casks (#546)

--- Casks/maa@beta.rb
+++ Casks/maa@beta.rb
@@ -1,22 +1,20 @@
-cask "maa-gui" do
+cask "maa@beta" do
   version "5.22.3"
   sha256 "56f03d304bccd7571711465b736c905941b58d29e31178ef29467619a37dcad2"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
       verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"
   name "MAA.app"
-  desc "GUI for MAA (MaaAssistantArknights)"
+  desc "Beta version of MAA (MaaAssistantArknights)"
   homepage "https://maa.plus/"
 
   livecheck do
     url :url
-    regex(/^v?(\d+\.\d+\.\d+)$/i)
+    regex(/^v?(\d+\.\d+\.\d+(?:-(?:beta|rc)\.\d+)?)$/i)
   end
 
-  deprecate! date: "2025-08-21", because: "install maa instead"
-
   auto_updates true
-  conflicts_with cask: "MaaAssistantArknights/tap/maa-gui-beta"
+  conflicts_with cask: "maa"
   depends_on macos: ">= :big_sur"
 
   app "MAA.app"

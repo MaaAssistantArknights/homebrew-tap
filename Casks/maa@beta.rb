ac605ff726603506f17a50340347f3a76e036cb9
Loong <git@loongw.cc>
2025-08-29 16:14:26 +0800

    maa@beta 5.24.0-beta.1

--- Casks/maa@beta.rb
+++ Casks/maa@beta.rb
@@ -1,6 +1,6 @@
 cask "maa@beta" do
-  version "5.23.2"
-  sha256 "f324169c51ea36adcc0319d242deaf63766943b9f3c5e578571bc4fe0802a6cb"
+  version "5.24.0-beta.1"
+  sha256 "c2b16e8cbde0b88854858ba8475ffedcd97018e873134462be3837e9ce131be9"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
       verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"

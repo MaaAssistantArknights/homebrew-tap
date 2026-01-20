56283f3b1b2c95edff939f036a2ecae5f7b8633a
Loong <40141251+wangl-cc@users.noreply.github.com>
2026-01-20 08:51:59 +0800

    maa@beta 6.3.0-beta.4 (#619)

--- Casks/maa@beta.rb
+++ Casks/maa@beta.rb
@@ -1,6 +1,6 @@
 cask "maa@beta" do
-  version "6.3.0-beta.3"
-  sha256 "087090c098637a523fe6967362437052af59d6156c53ca9d5b21fc5717dde428"
+  version "6.3.0-beta.4"
+  sha256 "dbf806dddae717bd2c0c51d9414f5894575c7e426759498759581033222662f3"
 
   url "https://github.com/MaaAssistantArknights/MaaAssistantArknights/releases/download/v#{version}/MAA-v#{version}-macos-universal.dmg",
       verified: "github.com/MaaAssistantArknights/MaaAssistantArknights/"

aee2e80de009eb7d10f827b564c86a306a4dc5a2
Loong <git@loongw.cc>
2026-05-30 16:05:07 +0100

    maa-cli 0.7.5

--- Formula/maa-cli-beta.rb
+++ Formula/maa-cli-beta.rb
@@ -1,21 +1,13 @@
 class MaaCliBeta < Formula
   desc "Command-line tool for MAA (MaaAssistantArknights)"
   homepage "https://github.com/MaaAssistantArknights/maa-cli/"
-  url "https://github.com/MaaAssistantArknights/maa-cli/archive/refs/tags/v0.7.4.tar.gz"
-  sha256 "21979d35ecb5a3a617e2e8b89acd4e9de1256b8f44c07ae63f6d4773bfee161f"
+  url "https://github.com/MaaAssistantArknights/maa-cli/archive/refs/tags/v0.7.5.tar.gz"
+  sha256 "3f288b98e783a4ff230982d5c235c179ccc97617b7da1fe61f6766412ea6badd"
   license "AGPL-3.0-only"
 
   livecheck do
     url :stable
     regex(/^v?(\d+\.\d+\.\d+(?:-(?:beta|rc)\.\d+)?)$/i)
-  end
-
-  bottle do
-    root_url "https://github.com/MaaAssistantArknights/homebrew-tap/releases/download/maa-cli-beta-0.7.4"
-    sha256 cellar: :any,                 arm64_tahoe:   "21861f63460cde7ede99ac9d25934cf5903e25d3cee6c9e8208f1b98a33fee35"
-    sha256 cellar: :any,                 arm64_sequoia: "8aa217948e1305cfab2e786b564be38146d5df7f580237e9e8b60a0e286f200b"
-    sha256 cellar: :any,                 arm64_sonoma:  "c9a6dbde65639580054185e32fc00f92c518a623df4559a8dfe65cafe14b1e88"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38fa12aad452dbe557effa66a7026a0a115b7e91531ca2a90dd837898253abf1"
   end
 
   option "without-git2", "Don't build with libgit2 resource updater backend"
@@ -44,7 +36,13 @@ class MaaCliBeta < Formula
 
     system "cargo", "install", "--no-default-features",
       "--features", features.join(","), *std_cargo_args(path: package_path)
-    fish_completion.install "#{package_path}/completions/maa.fish"
+    {
+      bash_completion/"maa"      => "bash",
+      zsh_completion/"_maa"      => "zsh",
+      fish_completion/"maa.fish" => "fish",
+    }.each do |completion, shell|
+      completion.write Utils.safe_popen_read({ "MAA_COMPLETE" => shell }, bin/"maa")
+    end
   end
 
   test do
--- Formula/maa-cli.rb
+++ Formula/maa-cli.rb
@@ -1,21 +1,13 @@
 class MaaCli < Formula
   desc "Command-line tool for MAA (MaaAssistantArknights)"
   homepage "https://github.com/MaaAssistantArknights/maa-cli/"
-  url "https://github.com/MaaAssistantArknights/maa-cli/archive/refs/tags/v0.7.4.tar.gz"
-  sha256 "21979d35ecb5a3a617e2e8b89acd4e9de1256b8f44c07ae63f6d4773bfee161f"
+  url "https://github.com/MaaAssistantArknights/maa-cli/archive/refs/tags/v0.7.5.tar.gz"
+  sha256 "3f288b98e783a4ff230982d5c235c179ccc97617b7da1fe61f6766412ea6badd"
   license "AGPL-3.0-only"
 
   livecheck do
     url :stable
     regex(/^v?(\d+\.\d+\.\d+)$/i)
-  end
-
-  bottle do
-    root_url "https://github.com/MaaAssistantArknights/homebrew-tap/releases/download/maa-cli-0.7.4"
-    sha256 cellar: :any,                 arm64_tahoe:   "815f239ca2dc3efcfa9767bdc6065eaa74a9c2cf37f13926ebfed2bcfcad15fe"
-    sha256 cellar: :any,                 arm64_sequoia: "c647ce4e9d63e7a43e0e974fcd96786c6b839f184e2eb39da7601ca593caae4b"
-    sha256 cellar: :any,                 arm64_sonoma:  "dde4ec9b33e5711deb2adb8d81d949a88a71883d1315407729bdf3fd37a55139"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc7b5d80d9472d1e078b992aa4aa759b54c2761df61e086d169d3a4c36b75f81"
   end
 
   option "without-git2", "Don't build with libgit2 resource updater backend"
@@ -45,7 +37,13 @@ class MaaCli < Formula
 
     system "cargo", "install", "--no-default-features",
       "--features", features.join(","), *std_cargo_args(path: package_path)
-    fish_completion.install "#{package_path}/completions/maa.fish"
+    {
+      bash_completion/"maa"      => "bash",
+      zsh_completion/"_maa"      => "zsh",
+      fish_completion/"maa.fish" => "fish",
+    }.each do |completion, shell|
+      completion.write Utils.safe_popen_read({ "MAA_COMPLETE" => shell }, bin/"maa")
+    end
   end
 
   test do
